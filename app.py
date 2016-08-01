837cf42c37c65450bd6c3fa5468165fcc8173f13
Nick Heinemans <nick@hostlogic.nl>
2016-08-01 11:21:35 +0200

    app.py aangepast

--- app.py
+++ app.py
@@ -6,10 +6,10 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-      provider = str(os.environ.get('PROVIDER', 'world'))
-        return 'Hello '+provider+'!'
+  provider = str(os.environ.get('PROVIDER', 'world'))
+  return 'Hello '+provider+'!'
 
-    if __name__ == '__main__':
-          # Bind to PORT if defined, otherwise default to 5000.
-            port = int(os.environ.get('PORT', 5000))
-              app.run(host='0.0.0.0', port=port)
+if __name__ == '__main__':
+  # Bind to PORT if defined, otherwise default to 5000.
+  port = int(os.environ.get('PORT', 5000))
+  app.run(host='0.0.0.0', port=port)
